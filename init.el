36ad19aae681161b6a3f3f673548b0a86f5fe6ac
Goblinlady <goblinmaiden1931@gmail.com>
2022-11-14 04:39:33 +0800

    add ivy to projectile

--- init.el
+++ init.el
@@ -210,6 +210,7 @@
 (use-package projectile
   :diminish projectile-mode
   :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
   :bind-keymap
   ("C-c p" . projectile-command-map)
   :init
