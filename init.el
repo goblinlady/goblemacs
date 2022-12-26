9f1c92018ec58a072b1a1a2ae9b02d422d82ce99
Goblinlady <goblinmaiden1931@gmail.com>
2022-12-26 20:55:13 +0800

    Chinese characters align problem in org table

--- init.el
+++ init.el
@@ -56,6 +56,18 @@
 		    :font "monospace"
 		    :height goblemacs/default-font-size)
 
+;; This part is stolen from:
+;; https://manateelazycat.github.io/emacs/2020/04/02/org-font.html
+(with-eval-after-load 'org
+  (defun org-buffer-face-mode-variable ()
+    (interactive)
+    (make-face 'width-font-face)
+    (set-face-attribute 'width-font-face nil :font "等距更纱黑体 SC 12")
+    (setq buffer-face-mode-face 'width-font-face)
+    (buffer-face-mode))
+
+  (add-hook 'org-mode-hook 'org-buffer-face-mode-variable))
+
 ;; Make ESC quit prompts
 ;; (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
